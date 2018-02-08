c249e49e7f171512854d782d30234206cfc112db
Drew Miley <drew.miley@opencastsoftware.com>
2018-02-08 11:18:09 +0000

    Adds SimpleObject

---
+++ src/life/drewmiley/SimpleObject.java
@@ -0,0 +1,20 @@
+package life.drewmiley;
+
+public class SimpleObject {
+
+    private int number;
+    private String text;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public SimpleObject(int number, String text) {
+        this.number = number;
+        this.text = text;
+    }
+}
