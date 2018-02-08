55fc443bf00e303ba0c11c677a7b81d7adbc665b
Drew Miley <drew.miley@opencastsoftware.com>
2018-02-08 13:46:39 +0000

    Adds forEach example

--- src/life/drewmiley/examples/ForEach.java
+++ src/life/drewmiley/examples/ForEach.java
@@ -2,23 +2,33 @@ package life.drewmiley.examples;
 
 import life.drewmiley.SimpleObject;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class ForEach {
 
-    public void runStreamForArray(SimpleObject[] simpleObjects) {
+    private void arbitaryFunction(SimpleObject simpleObject) {
+//        System.out.println(simpleObject.getNumber() + simpleObject.getText());
+    }
 
+    public void runStreamForArray(SimpleObject[] simpleObjects) {
+        Arrays.stream(simpleObjects).forEach(simpleObject -> arbitaryFunction(simpleObject));
     }
 
     public void runImperativeForArray(SimpleObject[] simpleObjects) {
-
+        for (int i = 0; i < simpleObjects.length; i++) {
+            SimpleObject simpleObject = simpleObjects[i];
+            arbitaryFunction(simpleObject);
+        }
     }
 
     public void runStreamForList(List<SimpleObject> simpleObjectList) {
-
+        simpleObjectList.forEach(simpleObject -> arbitaryFunction(simpleObject));
     }
 
     public void runImperativeForList(List<SimpleObject> simpleObjectList) {
-
+        for (SimpleObject simpleObject : simpleObjectList) {
+            arbitaryFunction(simpleObject);
+        }
     }
 }
