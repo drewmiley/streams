337e51f9760f7b64cb8aec992a53636414c6c672
Drew Miley <drew.miley@opencastsoftware.com>
2018-02-19 09:23:11 +0000

    Adds Dummy Unit Test

---
+++ test/life/drewmiley/examples/single/Map.java
@@ -0,0 +1,11 @@
+package life.drewmiley.examples.single;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Map {
+    @Test
+    public void firstTest() {
+        Assert.assertTrue(true);
+    }
+}
