a535efec46e87cb128655a36fab87987effee17d
Drew Miley <drew.miley@opencastsoftware.com>
2018-02-19 14:00:59 +0000

    Implements Map test with Stream Tester helper

--- test/life/drewmiley/examples/single/MapTest.java
+++ test/life/drewmiley/examples/single/MapTest.java
@@ -1,40 +1,30 @@
 package life.drewmiley.examples.single;
 
-import life.drewmiley.helper.ObjectGenerator;
-import org.junit.Before;
+import life.drewmiley.examples.StreamTester;
 import org.junit.Test;
 
 import java.util.List;
-import java.util.stream.IntStream;
 
-import static org.junit.Assert.assertTrue;
-
-public class MapTest {
+public class MapTest extends StreamTester {
 
     private Map map;
-    private ObjectGenerator objectGenerator;
 
-    @Before
-    public void before() {
+    public MapTest() {
+        super();
         map = new Map();
-        objectGenerator = new ObjectGenerator();
     }
 
     @Test
     public void arrayReturnsEqual() {
         String[] stream = map.runStreamForArray(objectGenerator.generateSimpleObjectArray());
         String[] imperative = map.runImperativeForArray(objectGenerator.generateSimpleObjectArray());
-        assertTrue(stream.length == imperative.length);
-        IntStream.range(1, stream.length)
-                .forEach(i -> assertTrue(stream[i].equals(imperative[i])));
+        testArraysEqual(stream, imperative);
     }
 
     @Test
     public void listReturnsEqual() {
         List<String> stream = map.runStreamForList(objectGenerator.generateSimpleObjectList());
         List<String> imperative = map.runImperativeForList(objectGenerator.generateSimpleObjectList());
-        assertTrue(stream.size() == imperative.size());
-        IntStream.range(1, stream.size())
-                .forEach(i -> assertTrue(stream.get(i).equals(imperative.get(i))));
+        testListsEqual(stream, imperative);
     }
 }
