d73f3e41f7eb24009f4aaa3635c8e78fec115062
Drew Miley <drewmiley92@gmail.com>
2018-03-30 20:20:47 +0100

    Implements FindFirstReduce

--- src/life/drewmiley/examples/chaining/FindFirstReduce.java
+++ src/life/drewmiley/examples/chaining/FindFirstReduce.java
@@ -1,24 +1,65 @@
 package life.drewmiley.examples.chaining;
 
 import life.drewmiley.helper.ComplexObject;
+import life.drewmiley.helper.InlineFunctions;
 import life.drewmiley.helper.SimpleObject;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FindFirstReduce {
     public SimpleObject runStreamForArray(ComplexObject[] complexObjects) {
-        return null;
+        return Arrays.stream(complexObjects)
+                .filter(complexObject -> InlineFunctions.arbitraryFilter(complexObject))
+                .findFirst()
+                .get().getSimpleObjectList().stream()
+                .reduce(InlineFunctions::arbitraryAccumulator)
+                .orElse(null);
     }
 
     public SimpleObject runImperativeForArray(ComplexObject[] complexObjects) {
-        return null;
+        ComplexObject[] array = complexObjects;
+        ComplexObject firstFound = null;
+        for (int i = 0; i < array.length; i++) {
+            ComplexObject complexObject = array[i];
+            if (InlineFunctions.arbitraryFilter(complexObject)) {
+                firstFound = complexObject;
+                break;
+            }
+        }
+        List<SimpleObject> simpleObjects = firstFound.getSimpleObjectList();
+        SimpleObject result = new SimpleObject(0, "");
+        for (SimpleObject simpleObject : simpleObjects) {
+            result = InlineFunctions.arbitraryAccumulator(result, simpleObject);
+        }
+        return result;
     }
 
-    public SimpleObject runStreamForList(List<ComplexObject> complexObjects) {
-        return null;
+    public SimpleObject runStreamForList(List<ComplexObject> complexObjectList) {
+        return complexObjectList.stream()
+                .filter(complexObject -> InlineFunctions.arbitraryFilter(complexObject))
+                .findFirst()
+                .get().getSimpleObjectList().stream()
+                .reduce(InlineFunctions::arbitraryAccumulator)
+                .orElse(null);
     }
 
-    public SimpleObject runImperativeForList(List<ComplexObject> complexObjects) {
-        return null;
+    public SimpleObject runImperativeForList(List<ComplexObject> complexObjectList) {
+        List<ComplexObject> list = complexObjectList;
+        ComplexObject firstFound = null;
+        for (ComplexObject complexObject : list) {
+            if (InlineFunctions.arbitraryFilter(complexObject)) {
+                firstFound = complexObject;
+                break;
+            }
+        }
+        List<SimpleObject> simpleObjects = firstFound.getSimpleObjectList();
+        SimpleObject result = new SimpleObject(0, "");
+        for (SimpleObject simpleObject : simpleObjects) {
+            result = InlineFunctions.arbitraryAccumulator(result, simpleObject);
+        }
+        return result;
     }
 }
